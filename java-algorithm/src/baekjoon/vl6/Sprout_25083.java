7264bfda503b194d2853aa3676af31aefecc30bb
moner050 <minhyung0237@gmail.com>
2024-08-28 13:19:36 +0900

    Feat: 25083번 새싹 문제 풀어보기

---
+++ java-algorithm/src/baekjoon/vl6/Sprout_25083.java
@@ -0,0 +1,26 @@
+package baekjoon.vl6;
+
+import java.io.*;
+
+public class Sprout_25083 {
+    public static void main(String[] args) {
+        //https://www.acmicpc.net/problem/25083
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        try {
+            bw.write("         ,r'\"7\n"+
+                    "r`-_   ,'  ,/\n" +
+                    " \\. \". L_r'\n" +
+                    "   `~\\/\n" +
+                    "      |\n" +
+                    "      |"
+            );
+
+            bw.close();
+        }
+        catch (Exception e) {
+            throw new RuntimeException("입력값이 잘못되었습니다.");
+        }
+    }
+}
