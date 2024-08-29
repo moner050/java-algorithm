d123a9628b944f5c24d499f9c02e8bbd331ca76f
moner050 <minhyung0237@gmail.com>
2024-08-29 17:23:10 +0900

    Feat: 2941번 크로아티아 알파벳 문제 풀어보기

---
+++ java-algorithm/src/baekjoon/vl6/CroatiaAlphabet_2941.java
@@ -0,0 +1,28 @@
+package baekjoon.vl6;
+
+import java.io.*;
+
+public class CroatiaAlphabet_2941 {
+    public static void main(String[] args) {
+        // https://www.acmicpc.net/problem/2941
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        try {
+            String[] croatiaWords = new String[] {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+            String input = br.readLine();
+
+            for (int i = 0; i < croatiaWords.length; i++) {
+                input = input.replaceAll(croatiaWords[i], String.valueOf(i));
+            }
+
+            bw.write(String.valueOf(input.length()));
+            bw.close();
+            br.close();
+        }
+        catch (Exception e) {
+            throw new RuntimeException("입력값이 잘못되었습니다.");
+        }
+    }
+}
