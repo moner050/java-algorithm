91640d64a2964f76b5032947de002680373bf219
moner050 <minhyung0237@gmail.com>
2025-04-27 17:41:24 +0900

    Feat: lv0 120803 두 수의 차 구하기 문제 풀어보기

---
+++ java-algorithm/src/programmers/lv0/Minus_120803.java
@@ -0,0 +1,12 @@
+package programmers.lv0;
+
+public class Minus_120803 {
+    // https://school.programmers.co.kr/learn/courses/30/lessons/120803
+    public static void main(String[] args) {
+        System.out.println(solution(2, 1));
+    }
+
+    public static int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+}
