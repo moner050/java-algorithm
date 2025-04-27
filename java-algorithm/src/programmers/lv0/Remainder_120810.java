b33c7a6b24e6813375c37aad7e9529568787cc6b
moner050 <minhyung0237@gmail.com>
2025-04-27 16:47:03 +0900

    Feat: lv0 120810 나머지 구하기 문제 풀어보기

---
+++ java-algorithm/src/programmers/lv0/Remainder_120810.java
@@ -0,0 +1,12 @@
+package programmers.lv0;
+
+public class Remainder_120810 {
+    // https://school.programmers.co.kr/learn/courses/30/lessons/120810?language=java
+    public static void main(String[] args) {
+        System.out.println(solution(3, 2));
+    }
+
+    public static int solution(int num1, int num2) {
+        return num1 % num2;
+    }
+}
