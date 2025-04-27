09a1077ab159629bf561fe30fa121950707f52af
moner050 <minhyung0237@gmail.com>
2025-04-27 17:44:06 +0900

    Feat: lv0 120802 두 수의 합 구하기 문제 풀어보기

---
+++ java-algorithm/src/programmers/lv0/Plus_120802.java
@@ -0,0 +1,12 @@
+package programmers.lv0;
+
+public class Plus_120802 {
+    // https://school.programmers.co.kr/learn/courses/30/lessons/120802
+    public static void main(String[] args) {
+        System.out.println(solution(1, 2));
+    }
+
+    public static int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+}
