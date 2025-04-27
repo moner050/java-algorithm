9e6bb3ddb62e8a54db196cf65cb2a5eba32cb160
moner050 <minhyung0237@gmail.com>
2025-04-27 17:39:41 +0900

    Feat: lv0 120804 두 수의 곱 문제 풀어보기

---
+++ java-algorithm/src/programmers/lv0/Multiplication_120804.java
@@ -0,0 +1,12 @@
+package programmers.lv0;
+
+public class Multiplication_120804 {
+    // https://school.programmers.co.kr/learn/courses/30/lessons/120804
+    public static void main(String[] args) {
+        System.out.println(solution(1, 3));
+    }
+
+    public static int solution(int num1, int num2) {
+        return num1 * num2;
+    }
+}
