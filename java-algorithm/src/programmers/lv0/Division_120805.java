9de6fdcdd14788a313de63bda8d636e9dd1e3916
moner050 <minhyung0237@gmail.com>
2025-04-27 17:43:00 +0900

    Feat: lv0 120805 두 수의 몫 구하기 문제 풀어보기

---
+++ java-algorithm/src/programmers/lv0/Division_120805.java
@@ -0,0 +1,12 @@
+package programmers.lv0;
+
+public class Division_120805 {
+    // https://school.programmers.co.kr/learn/courses/30/lessons/120805
+    public static void main(String[] args) {
+        System.out.println(solution(2, 1));
+    }
+
+    public static int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+}
