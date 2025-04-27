f20769fde7f63b3552ee4f793c1274751cbad8f7
moner050 <minhyung0237@gmail.com>
2025-04-27 17:37:57 +0900

    Feat: lv0 120807 숫자 비교하기 문제 풀어보기

---
+++ java-algorithm/src/programmers/lv0/CompareNumbers_120807.java
@@ -0,0 +1,11 @@
+package programmers.lv0;
+
+public class CompareNumbers_120807 {
+    // https://school.programmers.co.kr/learn/courses/30/lessons/120807
+    public static void main(String[] args) {
+        System.out.println(solution(2, 3));
+    }
+    public static int solution(int num1, int num2) {
+        return num1 == num2 ? 1 : -1;
+    }
+}
